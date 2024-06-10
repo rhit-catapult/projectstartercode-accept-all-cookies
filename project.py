8289fc00230204a4303bf9c3b897ef0a6da34d8c
jackbailey <jacknolanbailey@gmail.com>
2024-06-10 10:18:51 -0400

    Added test hashtags

--- project.py
+++ project.py
@@ -7,7 +7,7 @@ import time
 def main():
     # turn on pygame
     pygame.init()
-    #########
+    ##########
     # create a screen
     pygame.display.set_caption("Cool Project")
     # TODO: Change the size of the screen as you see fit!
