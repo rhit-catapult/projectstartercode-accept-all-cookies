24bcdb725148060b86e14dea68a1840bdc8c3b6b
jackbailey <jacknolanbailey@gmail.com>
2024-06-11 13:33:05 -0400

    Fix!

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     # turn on pygame
     pygame.init()
     # create a screen
-    pygame.display.set_caption("Cookie A Counselor")
+    pygame.display.set_caption("Cookie A Counselor")#
     screen_width = 1000
     screen_height = 600
     screen = pygame.display.set_mode((screen_width, screen_height))
